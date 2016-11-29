a0e42a35f62482812e18d5f9a3c22780bc206641
johnoke <okejohn.oluranti@gmail.com>
2016-11-29 10:39:19 +0100

    Example Fixes

--- SampleProject/Example.cs
+++ SampleProject/Example.cs
@@ -12,7 +12,7 @@ namespace SampleProject
     {
         static void Main(string[] args)
         {
-            IQuickTellerDriver driver = new QuickTellerDriver("IKIAF6C068791F465D2A2AA1A3FE88343B9951BAC9C3", "FTbMeBD7MtkGBQJw1XoM74NaikuPL13Sxko1zb0DMjI=", "3IWP0001");
+            IQuickTellerDriver driver = new QuickTellerDriver("ClientId", "ClientSecret", "TerminalId");
             //PaymentAdvice advice = new PaymentAdvice { amount ="10000", customerId = "08124266856", customerMobile = "08124266856", customerEmail = "08124266856", paymentCode = "90101", requestReference = "102920202", TerminalId = "3IWP0001" };
             Console.WriteLine(driver.GetCategories());
             Console.ReadKey();
